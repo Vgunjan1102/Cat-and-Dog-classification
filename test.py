fed3cc8701cbd8b3551fa21c79677c3b8a57b01a
Vgunjan1102 <89566891+Vgunjan1102@users.noreply.github.com>
2021-08-31 14:14:13 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from main import process_test_data
+# if you need to create the data:
+#test_data = process_test_data()
+# if you already have some saved:
+test_data = np.load('train_data.npy', allow_pickle=True)
+fig = plt.figure()
+IMG_SIZE = 50
+
+for num, data in enumerate(test_data[:12]):
+    # cat: [1,0]
+    # dog: [0,1]
+
+    img_num = data[1]
+    img_data = data[0]
+
+    y = fig.add_subplot(3, 4, num + 1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
+    # model_out = model.predict([data])[0]
+    model_out = model.predict([data])[0]
+
+    if np.argmax(model_out) == 1:
+        str_label = 'Dog'
+    else:
+        str_label = 'Cat'
+
+    y.imshow(orig, cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
+
+with open('submission_file.csv', 'w') as f:
+    f.write('id,label\n')
+with open('submission_file.csv', 'a') as f:
+    for data in tqdm(test_data):
+        img_num = data[1]
+        img_data = data[0]
+        orig = img_data
+        data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
+        model_out = model.predict([data])[0]
+        f.write('{},{}\n'.format(img_num, model_out[1]))
